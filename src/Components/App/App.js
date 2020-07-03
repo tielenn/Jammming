bc1b22685fc79a4fea8d8893c60356756b1aab37
tielenn <mcore.1212@gmail.com>
2020-07-03 22:45:36 +0300

    little changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,15 @@ class App extends React.Component {
             playlistName: 'My Playlist',
             playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
         }
+        this.addTrack = this.addTrack.bind(this);
+    }
+
+    addTrack(track) {
+        if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+            return;
+        }
+        this.state.playlistTracks.push(track);
+        this.setState({playlistTracks: this.state.playlistTracks})
     }
 
     render() {
@@ -21,7 +30,7 @@ class App extends React.Component {
               <div className="App">
                   <SearchBar />
                   <div className="App-playlist">
-                      <SearchResults searchResults={this.state.searchResults} />
+                      <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
                       <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
                   </div>
               </div>
